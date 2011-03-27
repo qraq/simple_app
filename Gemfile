b79f16269576e143ccdb5bea2e15644019222b5b
qraq <qrczaq997@wp.pl>
2011-03-27 10:55:19 -0700

    useer model + password, user show page

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ end
 group :test do
   gem 'rspec', '2.0.1'
   gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
+  gem 'gravatar_image_tag'
+
 end
 
 # Use unicorn as the web server
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,44 @@
+require 'digest'
+
 class User < ActiveRecord::Base
-attr_accessible(:name, :email)
+
+attr_accessible(:name, :email, :password, :password_confirmation)
+attr_accessor :password
 validates :name, :presence => true, :length => {:maximum =>50}
                 
   valid_mail =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :email, :presence => true, :format => {:with => valid_mail},
                   :uniqueness => { :case_sensitive => true }
-           
+validates :password, :presence => true, :confirmation => true,
+            :length => {:within => 6..40}
+          
+  
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+def has_password?(submitted_password)
+  encrypted_password  ==  encrypt(submitted_password)  
+end
+
+  
+  private 
+  
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+  
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+  
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+  
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+
 end
