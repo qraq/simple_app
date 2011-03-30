1f5e385330d94a4a33a0f1f8ecdb8c75917248d6
qraq <qrczaq997@wp.pl>
2011-03-30 09:39:35 -0700

    singup2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
   def new
-    @title = "Sign Up"
+  @user = User.new
+   @title = "Sign up"
   end
+
 
   def show
     @user = User.find(params[:id])
   end
+
+
+  def create
+  @user = User.new(params[:user])
+  if @user.save
+    flash[:success] = "Welcome to the Sample App!"
+    redirect_to @user
+  else
+    @title = "Sign up"
+    @user.password =""
+  
+      render 'new'
+    
+  end
+end
+
+
 end
